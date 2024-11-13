e1d1e51ad916d78237b297a368e5e4efe089200f
nathabee <nathalie.werner.fr@gmail.com>
2024-11-13 16:38:09 +0100

    frontend : login with parameter

--- competence-app/src/app/login/LoginForm.tsx
+++ competence-app/src/app/login/LoginForm.tsx
@@ -1,62 +1,71 @@
-'use client';
 // src/app/login/LoginForm.tsx
+'use client';
 
-
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
-import axios, { AxiosError } from 'axios'; 
+import React, { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import axios, { AxiosError } from 'axios';
 import { useAuth } from '../../context/AuthContext';
-
-import useFetchData from '@/hooks/useFetchData'; // Import your hook
+import useFetchData from '@/hooks/useFetchData';
 
 const LoginForm: React.FC = () => {
   const { login } = useAuth();
   const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState(''); 
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const router = useRouter();
-  const { fetchData } = useFetchData(); // Get the fetch function from the hook
+  const { fetchData } = useFetchData();
+  const searchParams = useSearchParams(); // Access URL parameters
+  
 
-  const handleLogin = async (e: React.FormEvent) => {
-    e.preventDefault();
+  useEffect(() => {
+
+
+    const user = searchParams.get('user');
+    if (user) {
+      setUsername(user);  
+      
+      const pwd = searchParams.get('password');
+      if (pwd) { 
+        setPassword(pwd); 
+      }
+      else {
+        setPassword('X');   // normally invalid because too small . For demo version it is not checked
+
+      }
+
+    }
+  }, [ ]);
+
+  const handleLogin = async (e?: React.FormEvent) => {
+    e?.preventDefault();
     try {
       const response = await axios.post(
         `${process.env.NEXT_PUBLIC_API_URL}/token/`,
         { username, password }
       );
 
-      const { access: token } = response.data; 
-
+      const { access: token } = response.data;
       const userResponse = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/users/me/`, {
         headers: { Authorization: `Bearer ${token}` },
       });
 
       const userInfo = userResponse.data;
-      console.log("userInfo",userInfo);
       login(token, userInfo);
-
-
-
-      // Call the fetch data function after login
-      // console.log("fetchData will be called")
-      await fetchData(); // Wait for the data fetching to complete
+      await fetchData();
       router.push(`/dashboard`);
     } catch (error) {
       const axiosError = error as AxiosError;
-      if (axiosError.response && axiosError.response.status === 401) {
-        setErrorMessage('Identifiant ou mot de passe invalide');
-      } else {
-        setErrorMessage('Erreur lors de la connection');
-      }
+      setErrorMessage(axiosError.response?.status === 401 ? 'Invalid username or password' : 'Connection error');
     }
   };
+ 
 
   return (
-    <div className="d-flex justify-content-center align-items-center min-vh-100 ">
+    <div className="d-flex justify-content-center align-items-center min-vh-100">
       <div className="card" style={{ maxWidth: '400px', width: '100%' }}>
         <div className="card-body">
           <h5 className="card-title text-center mb-4">Login</h5>
-          {errorMessage && <div className="alert alert-danger" role="alert">{errorMessage}</div>}
+          {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
           <form onSubmit={handleLogin}>
             <div className="mb-3">
               <label htmlFor="username" className="form-label">Identifiant</label>
@@ -66,9 +75,9 @@ const LoginForm: React.FC = () => {
                 className="form-control"
                 placeholder="Identifiant"
                 required
-                autoComplete="username"  
+                autoComplete="username"
                 value={username}
-                onChange={(e) => setUsername(e.target.value)} 
+                onChange={(e) => setUsername(e.target.value)}
               />
             </div>
             <div className="mb-3">
@@ -78,9 +87,9 @@ const LoginForm: React.FC = () => {
                 id="password"
                 className="form-control"
                 placeholder="Mot de passe"
-                value={password}
                 required
                 autoComplete="current-password"
+                value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
             </div>
@@ -93,4 +102,3 @@ const LoginForm: React.FC = () => {
 };
 
 export default LoginForm;
-
--- competence-app/src/app/login/page.tsx
+++ competence-app/src/app/login/page.tsx
@@ -1,32 +1,16 @@
+ 
+
 // src/app/login/page.tsx
 
-
+import { Suspense } from 'react';
 import LoginForm from '@/app/login/LoginForm';
 
 export default function LoginPage() {
-  return <LoginForm />;
-}
-
-/*
-'use client';
-import { useEffect, useState } from 'react';
-//import { useRouter } from 'next/navigation';
-import LoginForm from '@/app/login/LoginForm';
-
-export default function LoginPage() {
-  //const router = useRouter();
-  const [initialUsername, setInitialUsername] = useState<string | undefined>(); 
-
-  useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const username = urlParams.get('user'); 
-
-    // Only set initial values if they are provided in the URL
-    if (username) setInitialUsername(username); 
-  }, []);
-
   return (
-    <LoginForm initialUsername={initialUsername}  />
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginForm />
+    </Suspense>
   );
 }
-*/
+
+ 
